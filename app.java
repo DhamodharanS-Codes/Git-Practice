951d64888dd51551ef0624996c57ee6bee7a6697
Dhamodharan S <dhamodharans2001@gmail.com>
2026-06-04 10:06:26 +0530

    New line is printed

--- app.java
+++ app.java
@@ -4,6 +4,7 @@ public class App {
 
         System.out.println("Welcome to Git!");
         System.out.println("This is my first commit to GitHub.");
+        system.out.println("I am learning how to use Git and GitHub for version control.");
 
     }
 }
